8686cb3fbe52a5d716316dbd793136c04d82d825
RolandPG <meszaros.roland@diak.szbi-pg.hu>
2026-01-05 09:51:05 +0100

    Update test_simple_calculator_with_mock.py

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -1,5 +1,19 @@
+import pytest
+from simple_calculator import SimpleCalculator
+from unittest.mock import Mock
+
+
 def test_add_with_mock_history_service():
-    pass
+    mock_history = Mock()
+
+    calc = SimpleCalculator(history_service = mock_history)
+
+    result = calc.add(2, 3)
+
+    assert result == 5
+
+    mock_history.save_operation.assert_called_once()
+    mock_history.save_operation.assert_called_with("add", 2, 3, 5)
 
 def test_multiply_with_multiple_mocks():
     pass
